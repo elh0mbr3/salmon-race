9da670f7560686051cfd178e330a642bee701b79
cattywat <cattywat3@gmail.com>
2026-01-31 14:29:24 +0000

    madw a file

---
+++ backend/api.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def blank():
+    return "<p>nothing</p>"
+
+@app.route("/<file>")
+def page(file):
+    return "<p>"+file+"</p>"
