fcb76caf08f4e4875feeca2dc1e5df2030ac3984
cattywat <cattywat3@gmail.com>
2026-01-31 17:14:41 +0000

    made a thingy button

---
+++ src/app/components/buttons.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from "react";
+
+
+export default function Buttons(){
+    var thingymajiggywhatchamacallit;
+    return (<button onClick={() => {
+        thingymajiggywhatchamacallit = prompt("Select a salmon to bet on.");
+        if(thingymajiggywhatchamacallit!=null){
+            thingymajiggywhatchamacallit=parseInt(thingymajiggywhatchamacallit)
+            if(!isNaN(thingymajiggywhatchamacallit)&&0<thingymajiggywhatchamacallit&&thingymajiggywhatchamacallit<11){
+                console.log(thingymajiggywhatchamacallit-1);
+            }
+        }
+    }}>Place a bet</button>)
+}
