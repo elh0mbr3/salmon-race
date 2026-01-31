7d41b5f23194e9ac1064ee48bb88611d5c07c354
cattywat <cattywat3@gmail.com>
2026-01-31 17:56:26 +0000

    put the button in the div

--- src/app/components/buttons.tsx
+++ src/app/components/buttons.tsx
@@ -5,13 +5,17 @@ import React from "react";
 
 export default function Buttons(){
     var thingymajiggywhatchamacallit;
-    return (<button onClick={() => {
-        thingymajiggywhatchamacallit = prompt("Select a salmon to bet on.");
-        if(thingymajiggywhatchamacallit!=null){
-            thingymajiggywhatchamacallit=parseInt(thingymajiggywhatchamacallit)
-            if(!isNaN(thingymajiggywhatchamacallit)&&0<thingymajiggywhatchamacallit&&thingymajiggywhatchamacallit<11){
-                console.log(thingymajiggywhatchamacallit-1);
-            }
-        }
-    }}>Place a bet</button>)
+    return (<div>
+                <button onClick={() => {
+                    thingymajiggywhatchamacallit = prompt("Select a salmon to bet on.");
+                    if(thingymajiggywhatchamacallit!=null){
+                        thingymajiggywhatchamacallit=parseInt(thingymajiggywhatchamacallit)
+                        if(!isNaN(thingymajiggywhatchamacallit)&&0<thingymajiggywhatchamacallit&&thingymajiggywhatchamacallit<11){
+                            console.log(thingymajiggywhatchamacallit-1);
+                        }
+                    }
+                }}>
+                    Place a bet
+                </button>
+            </div>)
 }
